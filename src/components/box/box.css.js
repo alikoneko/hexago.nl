72a8c9bccce1401c4f6ddd90530cdd61a3b93dfa
ali neer <ali@alineer.com>
2019-08-30 01:40:31 -0400

    wider box

--- src/components/box/box.css.js
+++ src/components/box/box.css.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 2rem 4rem;
-  max-width: 700px;
+  max-width: 1000px;
   background-color: #fef7d2;
   color: #23212C;
   margin-top: 1em;
